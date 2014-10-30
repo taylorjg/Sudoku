ace5378a34f1b73aa49627ff58333c028574b7a2
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-10-30 14:14:59 +0000

    Initial commit

---
+++ src/Main.hs
@@ -0,0 +1,80 @@
+type Matrix a = [Row a]
+type Row a = [a]
+
+type Grid = Matrix Digit
+type Digit = Char
+
+digits :: [Digit]
+digits = ['1'..'9']
+
+blank :: Digit -> Bool
+blank = (== '0')
+
+solve :: Grid -> [Grid]
+solve = filter valid . expand . many prune . choices
+
+valid :: Grid -> Bool
+valid g =
+    all nodups (rows g) &&
+    all nodups (cols g) &&
+    all nodups (boxs g)
+
+nodups :: (Eq a) => [a] -> Bool
+nodups [] = True
+nodups (x:xs) = all (/= x) xs && nodups xs
+
+rows :: Matrix a -> Matrix a
+rows = id
+
+cols :: Matrix a -> Matrix a
+cols [xs] = [[x] | x <- xs]
+cols (xs:xss) = zipWith (:) xs (cols xss)
+
+boxs :: Matrix a -> Matrix a
+boxs =
+    map ungroup . ungroup .
+    map cols .
+    group . map group
+
+group :: [a] -> [[a]]
+group [] = []
+group xs = take 3 xs:group (drop 3 xs)
+
+ungroup :: [[a]] -> [a]
+ungroup = concat
+
+choices :: Grid -> Matrix [Digit]
+choices = map (map choice)
+
+choice :: Digit -> [Digit]
+choice d = if blank d then digits else return d
+
+expand :: Matrix [Digit] -> [Grid]
+expand = cp . map cp
+
+cp :: [[a]] -> [[a]]
+cp [] = [[]]
+cp (xs:xss) = [x:ys | x <- xs, ys <- yss]
+    where yss = cp xss
+
+prune :: Matrix [Digit] -> Matrix [Digit]
+prune = pruneBy rows . pruneBy cols . pruneBy boxs
+
+pruneBy :: (Matrix [Digit] -> Matrix [Digit]) -> Matrix [Digit] -> Matrix [Digit]
+pruneBy f = f . map pruneRow . f
+
+pruneRow :: Row [Digit] -> Row [Digit]
+pruneRow row = map (remove fixed) row
+    where fixed = [d | [d] <- row]
+
+remove :: [Digit] -> [Digit] -> [Digit]
+remove _ [x] = [x]
+remove fixed xs = filter (`notElem` fixed) xs
+
+many :: (Eq a) => (a -> a) -> a -> a
+many f x =  if x == x' then x else many f x'
+    where x' = f x
+
+main :: IO ()
+main = do
+    putStrLn "Hello"
